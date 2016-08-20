6d3443cfe79c83c4862d5fbf6d4ab8f10e4f3d4a
ejwalters <ejwalters24@gmail.com>
2016-08-20 14:50:22 -0400

    Initial Commit

---
+++ Favorite-Wine-08-20-2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorite-Wine-08-20-2016
+//
+//  Created by Eric Walters on 8/20/16.
+//  Copyright © 2016 Eric Walters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
